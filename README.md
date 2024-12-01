e8a27cbffb359b8d77c89ab9df20a01605421b52
Nemanja <nemanja.trajanovski.02@gmail.com>
2024-12-01 13:55:20 +0100

    'Lab1'

---
+++ README.md
@@ -0,0 +1,4 @@
+# MIS
+
+Mobile Information Systems Lab. Exercises - Мобилни Информациски Системи Лаб1. Вежби
+
---
+++ lib/data/clothing_data.dart
@@ -0,0 +1,52 @@
+import '../models/clothing_item.dart';
+
+final List<ClothingItem> clothingItems = [
+  ClothingItem(
+    name: 'Speedcross 6',
+    imageUrl: 'https://therunhub.ie/cdn/shop/files/Shopifytemplate-Ania-2024-01-23T093707.694_460x@2x.png?v=1706003122',
+    description: "Men's Shoes",
+    price: 145,
+  ),
+  ClothingItem(
+    name: 'Mountain Flex',
+    imageUrl: 'https://salomon.jp/cdn/shop/files/LC2327800_0_MOD_mountainflexjckt_surftheweb_outdoor_m.png?v=1722584874&width=850',
+    description: "Men's Jacket",
+    price: 220,
+  ),
+  ClothingItem(
+    name: 'Outerpath utility',
+    imageUrl: 'https://rs.beosport.com/media/catalog/product/cache/62e035a19ec38f27c8237d0c2b219b9a/L/C/LC2212400-02_1.jpg',
+    description: "Men's Pants",
+    price: 120,
+  ),
+  ClothingItem(
+    name: 'XT 15 Bladder',
+    imageUrl: 'https://mtnchalet.com/cdn/shop/files/71-1556350-19382821-image-1994857_640x640.png?v=1719527201',
+    description: "Men's Backpacks",
+    price: 91,
+  ),
+  ClothingItem(
+    name: 'Drx defy grvl',
+    imageUrl: 'https://salomonsports.co.za/cdn/shop/files/475641.jpg?v=1724320483',
+    description: "Men's Shoes",
+    price: 130,
+  ),
+  ClothingItem(
+    name: 'Trail Gaiters High',
+    imageUrl: 'https://www.theoutdoorshop.ie/cdn/shop/products/2_3c5cfe8b-af0c-4360-8f9a-ad8b02bd9690.png?v=1680615090&width=2048',
+    description: "Men's Accessories",
+    price: 45,
+  ),
+  ClothingItem(
+    name: 'Moon Patrol Gore-Tex',
+    imageUrl: 'https://comorsports.com/cdn/shop/files/210000090983SalomonMen_sMoonPatrolGore-texJacket2025WhitePepperGreenLflat.png?v=1723493972',
+    description: "Men's Snow Jacket",
+    price: 412,
+  ),
+  ClothingItem(
+    name: 'Crosstrak powder waterproof',
+    imageUrl: 'https://isusport.sk/wp-content/uploads/2024/10/trekingova-obuv-salomon-crosstrak-waterproof-l47570400-zelena-0000304035326_isusportsk.png',
+    description: "Men's Winter Shoes",
+    price: 200,
+  ),
+];
